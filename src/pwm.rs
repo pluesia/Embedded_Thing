353b4b9a3ac77f4a01a1b829dc59cdb8d42e8c13
Pablo Luesia <pluesia@github.com>
2019-03-28 01:16:09 +0100

    PWM done

--- src/pwm.rs
+++ src/pwm.rs
@@ -6,34 +6,13 @@
 extern crate panic_halt;
 extern crate stm32f4xx_hal as hal;
 
-
-use crate::hal::serial::{config::Config, Rx, Serial, Tx};
 use crate::hal::prelude::*;
 
-use stm32f4xx_hal::gpio::gpioa::PA5;
-use stm32f4xx_hal::gpio::AF1;
-use stm32f4xx_hal::gpio::Alternate;
-
-use hal::stm32::{ITM, TIM1, DWT};
 use cortex_m::{asm, iprintln};
 use rtfm::app;
-use nb::block;
 
 #[app(device = hal::stm32)]
 const APP: () = {
-
-    // static mut LED:hal::gpio::gpioa::PA5<hal::gpio::Output<hal::gpio::PushPull>> = ();
-    // static mut TIM1 : TIM1 = ();
-    // static mut RCC: hal::smt32::RCC = ();
-
-    
-    //static mut GPIOA : GPIOA = ();
-    //static mut GPIO : GPIO = ();
-    // init runs in an interrupt free section
-
-    // static mut TX: Tx<USART2> = ();
-    // static mut ITM: ITM = ();
-
 
     // Initialize the timer
     #[init]
@@ -49,52 +28,51 @@ const APP: () = {
 
         let gpioa = device.GPIOA.split();
 
-        let tx = gpioa.pa5.into_alternate_af5();
+        gpioa.pa8.into_alternate_af1();
+        gpioa.pa7.into_alternate_af1(); 
 
-        tim1.cr1.write(|w| w.cen().set_bit());
+        // Output compare mode
+        tim1.ccmr1_output.write(|w| w.oc1pe().set_bit() );
+        // PWM mode
+        unsafe{ tim1.ccmr1_output.write(|w| w.oc1m().bits(0b110)) };
+        // Upcounting
+        tim1.cr1.write(|w| w.dir().clear_bit());
 
+        // Enable all OC and OCN
+        tim1.bdtr.write(|w| w
+            .moe().set_bit()
+            .ossr().set_bit()        
+        );
+        tim1.ccer.write(|w| w
+            .cc1ne().set_bit()      // OC1N signal is output
+            .cc1e().set_bit()       // OC1 signal is output
+        );
 
-        // Setting the prescaler value
-        unsafe{ tim1.psc.write(|w| w.psc().bits(79)) };
-
-        // Setting the capture/compare register 1
-        unsafe{ tim1.ccr1.write(|w| w.ccr1().bits(0x0100))};
-
-        // Setting the auto-reload register
-        unsafe{ tim1.arr.write(|w| w.arr().bits(0x1111)) };
-
-        // Setting counting mode to uppcounting mode 0 to value
-        unsafe {tim1.cr1.write(|w| w.urs().set_bit() ) };
-        unsafe {tim1.cr1.write(|w| w.arpe().enabled() ) };
-        unsafe {tim1.cr1.write(|w| w.cen().enabled() ) };
-
-        // Setting the counter value
-        unsafe{ tim1.cnt.write(|w| w.cnt().bits(0x0000) ) };    
-        // Output compare mode 1 enabled, enabled PWM
-        unsafe{ tim1.ccmr1_output.write(|w| w.oc1m().bits(0b110) ) };
-
-        unsafe{ tim1.ccmr1_output.write(|w| w.oc1pe().set_bit() ) };
-
-        unsafe{ tim1.ccer.write(|w| w.cc1np().set_bit() ) };
-
-        //unsafe{ tim1.ccer.write(|w| w..enabled() };
+        // Enable the interrupt on co1
+        tim1.dier.modify(|_, w| w.cc1ie().set_bit());       
         
-        unsafe{ tim1.egr.write(|w| w.ug().update() ) };
+        unsafe{     // Prescale, counter, reload value, duty cycle
+            // Setting the prescaler value
+            tim1.psc.write(|w| w.psc().bits(0x0));
+            // Setting the capture/compare register 1
+            tim1.ccr1.write(|w| w.ccr1().bits(0x4000));
+            // Setting the auto-reload register
+            tim1.arr.write(|w| w.arr().bits(0xFFFF));
+            // Setting the counter value
+            tim1.cnt.write(|w| w.cnt().bits(0x0) )  
+        }; 
 
-        // RCC = rcc;
-        // TIM1 = tim1;  
-        // LED = led;
-        // TX = tx;
-        // ITM = core.ITM;
+        tim1.cr1.write(|w| w.cen().set_bit());      // Counting start
 
+        // tim1.cr1.write(|w| w.arpe().set_bit());  // DO NOT USE THIS
+        tim1.egr.write(|w| w.ug().update() );   
     }
 
-    // #[idle(resources = [TX, ITM])]
     #[idle]
     fn idle() -> ! {
-        // let tx = resources.TX;
-        // let stim = &mut resources.ITM.stim[0];
-        loop{}
+        loop{
+            asm::wfi();
+        }
     }
 
     #[interrupt]
@@ -104,8 +82,8 @@ const APP: () = {
     #[interrupt]
     fn USART2() {}
 
-    #[interrupt()]
+    #[interrupt(priority = 3)]
     fn TIM1_CC() {
-        asm::nop()
+        asm::nop();      
     }
 };
